6e7b5c344b80e9d714b5a34329c8297f4266ca44
CyberSecurityHat <godofwhiteworld@gmail.com>
2023-09-17 22:43:00 +0900

    2023-09-17_CyberSecurityHat

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,24 @@ const path = require("path");
 
 const router = express.Router();
 
-const XSS_vulnerability_site = require("./XSS_vulnerability_site")
+const reflected_xss_vulnerability_site = require("./reflected_xss_vulnerability_site")
 const pathtraversal_vulnerability_site = require("./pathtraversal_vulnerability_site")
 const OS_command_injection = require("./OS_command_injection")
+const stored_xss_vulnerability_site = require("./stored_xss_vulnerability_site.js")
+const stored_xss_vulnerability_site_board = require("./stored_xss_vulnerability_site_board.js")
 
 router.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
-router.use("/xss", XSS_vulnerability_site);
+router.use("/reflected_xss", reflected_xss_vulnerability_site);
 
 router.use("/path_traversal", pathtraversal_vulnerability_site)
 
 router.use("/os_command_injection", OS_command_injection)
+
+router.use("/stored_xss", stored_xss_vulnerability_site)
+
+router.use("/stored_xss/board", stored_xss_vulnerability_site_board)
 
 module.exports = router;
